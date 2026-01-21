849b7df428132da435c0cc1f98dc2266d4b27789
Zachary Smith <zachary.smith@huttoisd.org>
2026-01-21 17:35:21 -0600

    Attempt Download

--- src/FFMPEG_handler.py
+++ src/FFMPEG_handler.py
@@ -10,6 +10,25 @@ def download_ffmpeg():
         try:
             subprocess.run(['ffmpeg'])
         except FileNotFoundError:
-            ffmpeg_download = requests.get("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.7z")
+            print("Downloading ffmpeg to home/ffmpeg folder...")
+            import patoolib
+            patoolib.extract_archive("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.7z", outdir=ffmpeg_path)
     else:
-        print("ERROR: Unable to Find FFMPEG. This isn't a Windows system so ")
+        print("ERROR: Unable to Find FFMPEG. This isn't a Windows system so you will have to install it yourself.")
+        
+# Source - https://stackoverflow.com/a/16696317
+# Posted by Roman Podlinov, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-01-21, License - CC BY-SA 4.0
+
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192): 
+                # If you have chunk encoded response uncomment if
+                # and set chunk_size parameter to None.
+                #if chunk: 
+                f.write(chunk)
+    return local_filename
--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import keyboard
 import time
 import PyVisualHelp
+import FFMPEG_handler
     
 keyboard.add_hotkey('ctrl+\\', PyVisualHelp.take_and_show_screenshot)
+
+FFMPEG_handler.download_ffmpeg()
 
 while True:
     time.sleep(1)
