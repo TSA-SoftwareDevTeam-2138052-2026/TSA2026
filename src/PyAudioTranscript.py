dbe1940edb1402f64e010b87781183f09eb977b1
Zachary Smith <zachary.smith@huttoisd.org>
2026-01-28 17:56:16 -0600

    Add basic transcript making and comment out demo

--- src/PyAudioTranscript.py
+++ src/PyAudioTranscript.py
@@ -1,10 +1,20 @@
-import speech_recognition as sr
-recog = sr.Recognizer()
+#import speech_recognition as sr
+#recog = sr.Recognizer()
+#
+#from os import path
+#audio = sr.AudioData.from_file("demo_audio.wav")
 
-from os import path
-audio = sr.AudioData.from_file("demo_audio.wav")
+#try:
+#    print(recog.recognize_google(audio))
+#except:
+#    print("ERROR")
 
-try:
-    print(recog.recognize_google(audio))
-except:
-    print("ERROR")
+# Real program here
+
+class PyAudioTranscript():
+    recognizer = sr.Recognizer()
+    
+    @classmethod
+    def turn_into_transcript(cls, audio_file: str) -> str:
+        audio = sr.AudioData.from_file(audio_file)
+        return cls.recognizer.recognize_google(audio)
--- src/main.py
+++ src/main.py
@@ -8,7 +8,11 @@ keyboard.add_hotkey('ctrl+\\', screenshot.take_and_show_screenshot)
 ffmpeg_path = ffmpeg.download_ffmpeg()
 print(ffmpeg_path)
 
-ffmpeg.remove_video(input()) # demo for now, we will add actual file dialogs later.
+path_to_vid = ffmpeg.remove_video(input()) # demo for now, we will add actual file dialogs later.
+
+from PyAudioTranscript import PyAudioTranscript
+
+print(PyAudioTranscript.turn_into_transcript(path_to_vid))
 
 while True:
     time.sleep(1)
