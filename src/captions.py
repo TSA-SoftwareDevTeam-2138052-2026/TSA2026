b488fd314baf8560defeec485b821a0f9085d47d
Zachary Smith <zachary.smith@huttoisd.org>
2026-02-17 11:21:30 -0600

    Add caption timing

--- src/captions.py
+++ src/captions.py
@@ -9,13 +9,14 @@ class Captions:
             current_captions = "WEBVTT\n\n"
             is_in_segment = False
             is_first = False
-            for line in temp.split("\n"):
+            for line in temp.splitlines():
                 if line.endswith(":") and not line.startswith("  "):
-                    current_captions = "WEBVTT"
+                    current_captions = current_captions + line.split(":")[0]
                     is_in_segment = True
                     is_first = True
                 if line.startswith("  ") and is_in_segment:
                     if is_first:
+                        print("first")
                         time = line.split(":")[0].split("/")[0]
                         seconds_left = float(time)
                         minutes = 0
@@ -29,4 +30,9 @@ class Captions:
                                 if minutes - 60 <= 0:
                                     minutes -= 0
                                     hours += 1
-                        current_captions = current_captions + f"{hours:.2}"
+                        if len(str(hours)) <= 2:
+                            current_captions = current_captions + f"{hours:02}:{minutes:02}:{seconds_left:.3f}"
+                        else:
+                            current_captions = current_captions + f"{hours}:{minutes:02}:{seconds_left:.3f}"
+                        
+        return current_captions
